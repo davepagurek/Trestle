e5aa2fd13f7a3e67e0f2dd30d52a60023b36dff0
Dave Pagurek <dave@davepagurek.com>
2014-10-31 12:49:46 -0400

    Updated source meta to JSON

--- index.pl
+++ index.pl
@@ -5,7 +5,7 @@ use strict;
 
 my %config = do 'config.pl';
 my $query = CGI->new();
-my $pageName = $query->param("page") || "programming/xcalc-js";
+my $pageName = $query->param("page") || "film/the-weight";
 my $source = "content/" . $pageName . ".html";
 my $sourceDir = "content/" . $pageName;
 my $sourceCache = "cache/" . $pageName . "_cache.html";
