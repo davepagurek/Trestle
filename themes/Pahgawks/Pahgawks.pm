948a55d3900ccbf33913729632341a48918b2c85
Dave Pagurek <dave@pahgawks.com>
2014-11-01 13:13:08 -0400

    Working theme pages

--- themes/Pahgawks/Pahgawks.pm
+++ themes/Pahgawks/Pahgawks.pm
@@ -4,7 +4,6 @@ use strict;
 
 use lib "../..";
 use Page;
-use Time::Piece;
 
 sub new {
 	my $class = shift;
@@ -83,11 +82,9 @@ sub content {
 	  <div class='wrapper'>\n";
 
 	$source .= "<h1>" . $page->meta("title") . "</h1>\n";
-	my $date = Time::Piece->strptime($page->meta("date"), "%Y-%m-%d");
-	$source .= "<div id='date'>" . $date->mday . " " . $date->fullmonth . ", " . $date->year . "</div>\n";
+	$source .= "<div id='date'>" . $page->meta("date")->mday . " " . $page->meta("date")->fullmonth . ", " . $page->meta("date")->year . "</div>\n";
 
 	if ($page->meta("awards")) {
-		my @awards = split(",", $page->meta("awards"));
 		$source .= "<div class='awards_full'>\n<table>\n";
 		foreach my $award (@{ $page->meta("awards") }) {
 			$source .= "<tr>
@@ -115,62 +112,103 @@ sub content {
 }
 
 sub dir {
-	my ($self, $sourceDir, $root) = @_;
-	my @pages = ();
+	my ($self, $root, @pages) = @_;
 	my $source = "";
-	foreach my $pageFile (glob("$sourceDir/*")) {
-		push(@pages, Page->new($pageFile, $root));
-	}
 	
 	my $title = "Test";
 
-	# $source .= "<html>
-	# <head>
-	# <title>" .
-	# 	$title . 
-	# 	"</title>
-	# 	<link href='http://fonts.googleapis.com/css?family=Bitter:400' rel='stylesheet' type='text/css'>
-	# 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,300' rel='stylesheet' type='text/css'>
-	# 	<link href='" . $root . "/themes/Pahgawks/Pahgawks.css' rel='stylesheet' type='text/css'>
-	# 	</head>
-	# 	<body>
-	# 	<div id='title'><h1><a href='http://www.pahgawks.com'>Dave Pagurek</a></h1></div>
-	# 	<div class='section' id='menu'>
-	# 	  <a id='menuLabel' onclick=\"(document.getElementById('menu').className=='section')?document.getElementById('menu').className='section open':document.getElementById('menu').className='section';\">Menu</a>
-	# 	  <div class='wrapper'>
-	# 	    <a href='http://www.pahgawks.com' class='title'>Dave Pagurek</a>
-	# 	      <div id='links'>
-	# 	      <a href='http://www.pahgawks.com'>About</a>
-	# 	      <a href='http://www.pahgawks.com/blog'>Blog</a>
-	# 	      <a href='http://www.pahgawks.com/archives' class='selected'>Portfolio</a>
-	# 	    </div>
-	# 	  </div>
-	# 	</div>";
+	$source .= "<html>
+	<head>
+	<title>" .
+		$title . 
+		"</title>
+		<link href='http://fonts.googleapis.com/css?family=Bitter:400' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,300' rel='stylesheet' type='text/css'>
+		<link href='" . $root . "/themes/Pahgawks/Pahgawks.css' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+		<div id='title'><h1><a href='http://www.pahgawks.com'>Dave Pagurek</a></h1></div>
+		<div class='section' id='menu'>
+		  <a id='menuLabel' onclick=\"(document.getElementById('menu').className=='section')?document.getElementById('menu').className='section open':document.getElementById('menu').className='section';\">Menu</a>
+		  <div class='wrapper'>
+		    <a href='http://www.pahgawks.com' class='title'>Dave Pagurek</a>
+		      <div id='links'>
+		      <a href='http://www.pahgawks.com'>About</a>
+		      <a href='http://www.pahgawks.com/blog'>Blog</a>
+		      <a href='http://www.pahgawks.com/archives' class='selected'>Portfolio</a>
+		    </div>
+		  </div>
+		</div>
+		<div class='section top' id='content'>
+		<div class='wrapper'>
+		<h1 class='cat'>" . $title . "</h1>
+		<p>
+		<a href='http://www.davepagurek.com/film' class='cat'>Animation</a>
+		<a href='http://www.davepagurek.com/programming' class='cat'>Programming</a>
+		<a href='http://www.davepagurek.com/art' class='cat'>Art</a>
+		<a href='http://www.davepagurek.com/music' class='cat'>Music</a>
+		<a href='http://www.davepagurek.com/blog' class='cat'>Blog</a>
+		<a href='http://www.davepagurek.com/archives' class='cat'>Everything</a>
+		</p>";
+
+	my $oldYear = 0;
+	my $yearNum = 0;
 	foreach my $page (@pages) {
-		#$source .= "<div>" . $page->meta("title") . "</div>";
-		$source .= $page->meta("source") . "\n";
+		if ($yearNum != $page->meta("date")->year) {
+			$oldYear = $page->meta("date")->year;
+			$yearNum++;
+			$source .= "</div>
+				</div>
+				<div class='section archive" . ($yearNum%2==0?"":" odd") . "' id='content'>
+				<div class='wrapper icons'>
+				<h2>" . $page->meta("date")->year . "</h2>";
+		}
+
+		$source .= "<div class='animation'><div class='icon' style='background-image:url(" . $page->meta("thumbnail") . ")'><a href=" . $page->meta("url") . "></a></div><div class='info'><a class='title' href='" . $page->meta("url") . "''>" . $page->meta("title") . "</a>";
+
+		if ($page->meta("awards")) {
+			$source .= "<div class='awards'>\n";
+			foreach my $award (@{ $page->meta("awards") }) {
+				$source .= "<div class='" . $award->{award} . "' title='" . $award->{description} . "''></div>";
+			}
+			$source .= "</div>\n";
+		}
+
+		if ($page->meta("languages")) {
+			$source .= "<div class='languages'>Made with ";
+			my $j = 0;
+			foreach my $language (@{ $page->meta("languages") }) {
+				$source .= $language;
+				if (scalar @{ $page->meta("languages") } == 2 && $j == 0) {
+					$source .= " and ";
+				} elsif ($j == scalar @{ $page->meta("languages") }-2) {
+					$source .= ", and ";
+				} elsif ($j<scalar @{ $page->meta("languages") }-1) {
+					$source .= ", ";
+				}
+				$j++;
+			}
+			$source .= "</div>";
+		}
+
+		$source .= "<p>" . $page->meta("excerpt") . "</p>";
+		$source .= "<div class='date'>" . $page->meta("date")->mday . " " . $page->meta("date")->fullmonth . ", " . $page->meta("date")->year . "</div>
+			</div>
+			</div>";
 	}
-	
-	# push(@pages, "a");
-	# push(@pages, "b");
-	# push(@pages, "c");
-	# foreach my $page (@pages) {
-	# 	$source .= $page . "\n";
-	# }
 
-
-	# $source .= "</div>
-	# 	</div>
-	# 	<div class='section' id='footer'>
-	# 	  <div class='wrapper'>
-	# 	    <p>My name is Dave and I'm an animator, musician, programmer, designer and artist.</p>
-	# 	    <p>Want to get in touch? Find/contact me elsewhere:</p>
-	# 	    <p><a href='mailto:dave\@pahgawks.com' class='button'>dave\@pahgawks.com</a> <a href='http://davepvm.tumblr.com/' class='button external' target='_blank'>Tumblr</a> <a href='http://pahgawk.deviantart.com/' class='button external' target='_blank'>DeviantART</a> <a href='http://pahgawk.newgrounds.com/' class='button external' target='_blank'>Newgrounds</a> <a href='http://www.youtube.com/pahgawk' class='button external' target='_blank'>YouTube</a> <a href='http://www.twitter.com/davepvm' class='button external' target='_blank'>Twitter</a> <a href='http://pahgawks.bandcamp.com/' class='button external' target='_blank'>Bandcamp</a> <a href='http://soundcloud.com/davidpvm' class='button external' target='_blank'>Soundcloud</a> <a href='https://github.com/pahgawk/' class='button external' target='_blank'>GitHub</a>
-	# 	      </p>
-	# 	    </div>
-	# 	</div>
-	# 	</body>
-	# 	</html>\n";
+	$source .= "</div>
+		</div>
+		<div class='section' id='footer'>
+		  <div class='wrapper'>
+		    <p>My name is Dave and I'm an animator, musician, programmer, designer and artist.</p>
+		    <p>Want to get in touch? Find/contact me elsewhere:</p>
+		    <p><a href='mailto:dave\@pahgawks.com' class='button'>dave\@pahgawks.com</a> <a href='http://davepvm.tumblr.com/' class='button external' target='_blank'>Tumblr</a> <a href='http://pahgawk.deviantart.com/' class='button external' target='_blank'>DeviantART</a> <a href='http://pahgawk.newgrounds.com/' class='button external' target='_blank'>Newgrounds</a> <a href='http://www.youtube.com/pahgawk' class='button external' target='_blank'>YouTube</a> <a href='http://www.twitter.com/davepvm' class='button external' target='_blank'>Twitter</a> <a href='http://pahgawks.bandcamp.com/' class='button external' target='_blank'>Bandcamp</a> <a href='http://soundcloud.com/davidpvm' class='button external' target='_blank'>Soundcloud</a> <a href='https://github.com/pahgawk/' class='button external' target='_blank'>GitHub</a>
+		      </p>
+		    </div>
+		</div>
+		</body>
+		</html>\n";
 
 
 	return $source;
