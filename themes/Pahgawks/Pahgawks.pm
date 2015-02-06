49632345137ab11fa29dbfbf9a8db90c33c2fccd
Dave Pagurek <dave@pahgawks.com>
2015-02-06 00:59:14 -0500

    Templating using HTML::Template

--- themes/Pahgawks/Pahgawks.pm
+++ themes/Pahgawks/Pahgawks.pm
@@ -3,158 +3,56 @@ package Pahgawks;
 use strict;
 
 use lib "../..";
+use HTML::Template;
 use Page;
 
 sub new {
     my $class = shift;
-    my $self = { };
+    my $self = {
+        dir => "themes/Pahgawks"
+    };
 
     bless $self, $class;
     return $self;
 }
 
-sub footer {
-    my $self = shift;
-
-    my $source = "<div class='section' id='footer'>
-    <div class='wrapper'>
-    <p>My name is Dave and I'm an animator, musician, programmer, designer and artist.</p>
-    <p>Want to get in touch? Find/contact me elsewhere:</p>
-    <p>
-    <a href='mailto:dave\@davepagurek.com' class='button'>dave\@davepagurek.com</a>
-    <a href='https://github.com/pahgawk/' class='button external' target='_blank'>GitHub</a>
-    <a href='http://codepen.io/davepvm/' class='button external' target='_blank'>CodePen</a>
-    <a href='http://pahgawk.newgrounds.com/' class='button external' target='_blank'>Newgrounds</a>
-    <a href='http://www.youtube.com/pahgawk' class='button external' target='_blank'>YouTube</a>
-    <a href='http://pahgawks.bandcamp.com/' class='button external' target='_blank'>Bandcamp</a>
-    <a href='http://soundcloud.com/davidpvm' class='button external' target='_blank'>Soundcloud</a>
-    </p>
-    </div>
-    </div>
-    </body>
-    </html>\n";
-
-    return $source;
-}
-
-sub header {
-    my ($self, $category, $title, $root) = @_;
-
-    $category = lc($category);
-
-    my $source = "<html>
-    <head>
-    <title>" .
-    (($category eq "about")?$title:"$title - Dave Pagurek") .
-    "</title>
-    <link href='http://fonts.googleapis.com/css?family=Bitter:400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,300' rel='stylesheet' type='text/css'>
-    <meta http-equiv='X-UA-Compatible' content='IE=Edge'/>
-    <meta name='theme-color' content='#e74c3c' />
-    <meta name='viewport' content='width=device-width' />
-    <link href='" . $root . "/themes/Pahgawks/Pahgawks.css' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-    <div id='title'><h1><a href='http://www.davepagurek.com'>Dave Pagurek</a></h1></div>
-    <div class='section' id='menu'>
-    <a id='menuLabel' onclick=\"(document.getElementById('menu').className=='section')?document.getElementById('menu').className='section open':document.getElementById('menu').className='section';\">Menu</a>
-    <div class='wrapper'>
-    <a href='http://www.davepagurek.com' class='title'>Dave Pagurek</a>
-    <div id='links'>
-    <a href='" . $root . "'" . ($category eq "about" ? " class='selected'" : "") . ">About</a>
-    <a href='" . $root . "/blog'" . ($category eq "blog" ? " class='selected'" : "") . ">Blog</a>
-    <a href='" . $root . "/archives'" . (!($category eq "about" || $category eq "blog" || $category eq "error") ? " class='selected'" : "") . ">Portfolio</a>
-    </div>
-    </div>
-    </div>";
-
-    return $source;
-}
 
 sub content {
     my ($self, $page) = @_;
-    my $source = "";
+    my $template = HTML::Template->new(
+        filename => "themes/Pahgawks/template/content.tmpl",
+        die_on_bad_params => 0
+    );
 
-    $source .= $self->header($page->meta("category"), $page->meta("title"), $page->meta("root"));
-    if ($page->meta("youtube")) {
-        $source .= "<div class='section' id='video'>
-        <div class='wrapper'>
-        <iframe class='youtube' width='560' height='315' src='http://www.youtube.com/embed/" . $page->meta("youtube") . "?rel=0' frameborder='0' allowfullscreen></iframe>
-        </div>
-        </div>\n";
-    } elsif ($page->meta("browser") || $page->meta("android") || $page->meta("embed") || $page->meta("video") || $page->meta("buttons") || $page->meta("art")) {
-        $source .= "<div class='section odd'>
-        <div class='wrapper centered'>";
+    $template->param(
+        title => $page->template("title"),
+        category => $page->template("category"),
+        content => $page->content,
+        date => $page->template("date"),
+        awards => $page->template("awards"),
+        isAbout => $page->template("category") eq "about",
+        isBlog => $page->template("category") eq "blog",
+        isPortfolio => !($page->template("category") eq "about" || $page->template("category") eq "blog" || $page->template("category") eq "error"),
+        root => $page->template("root"),
+        youtube => $page->template("youtube"),
+        header => ($page->template("browser") || $page->template("android") || $page->template("embed") || $page->template("video") || $page->template("buttons") || $page->template("art")),
+        art => $page->template("art"),
+        embed => $page->template("embed"),
+        video => $page->template("video"),
+        buttons => $page->template("buttons"),
+        browser => $page->template("browser"),
+        android => $page->template("android"),
+    );
 
-        if ($page->meta("browser")) {
-            $source .= "<div class='browser big'>
-            <div class='winbutton'></div>
-            <div class='winbutton'></div>
-            <div class='winbutton'></div>
-            <div class='navbar'>" . $page->meta("browser")->{url} . "</div>
-            <a href='" . $page->meta("browser")->{url} . "'>
-            <img src='" . $page->meta("browser")->{image} . "' />
-            </a>
-            </div>";
-        }
-        if ($page->meta("android")) {
-            $source .= "<div class='android big'>
-            <div class='volume'></div>
-            <div class='power'></div>
-            <a href='" . $page->meta("android")->{url} . "'>
-            <img src='" . $page->meta("android")->{image} . "' />
-            </a>
-            </div>";
-        }
-        if ($page->meta("art")) {
-            $source .= "<a href='" . $page->meta("art") . "'><img src='" . $page->meta("art") . "' class='art' /></a>";
-        }
-        if ($page->meta("embed")) {
-            $source .= $page->meta("embed");
-        }
-        if ($page->meta("video")) {
-            $source .= $page->meta("video");
-        }
-        if ($page->meta("buttons")) {
-            $source .= "<p>";
-            foreach my $button (@{ $page->meta("buttons") }) {
-                $source .= "<a class='button' href='" . $button->{url} . "'>" . $button->{text} . "</a>";
-            }
-            $source .= "</p>";
-        }
+    return $template->output;
 
-        $source .= "</div>
-        </div>\n";
-    }
-    $source .= "<div class='section' id='content'>
-    <div class='wrapper'>\n";
-
-    $source .= "<h1>" . $page->meta("title") . "</h1>\n";
-    $source .= "<div id='date'>" . $page->meta("date")->{mday} . " " . $page->meta("date")->{fullmonth} . ", " . $page->meta("date")->{year} . "</div>\n";
-
-    if ($page->meta("awards")) {
-        $source .= "<div class='awards_full'>\n<table>\n";
-        foreach my $award (@{ $page->meta("awards") }) {
-            $source .= "<tr>
-            <th><div class='" . $award->{award} . "' title='" . $award->{description} . "''></div></th>
-            <td> " . $award->{description} . "</td>
-            </tr>\n";
-        }
-        $source .= "</table>\n</div>\n";
-    }
-
-    $source .= $page->content;
-    $source .= "</div>
-    </div>";
-    $source .= $self->footer();
-    return $source;
 }
 
 sub dir {
     my ($self, $category) = @_;
     my $source = "";
 
-    $source .= $self->header($category->info("name"), $category->info("name"), $category->info("root"));
+    #$source .= $self->header($category->info("name"), $category->info("name"), $category->info("root"));
     $source .= "<div class='section top' id='content'>
     <div class='wrapper'>
     <h1 class='cat'>" . $category->info("name") . "</h1>
@@ -215,7 +113,7 @@ sub dir {
 
     $source .= "</div>
     </div>";
-    $source .= $self->footer();
+    #$source .= $self->footer();
 
 
     return $source;
@@ -227,7 +125,7 @@ sub archives {
 
     my @categories = sort { $a->info("rank") <=> $b->info("rank") } @cats;
 
-    $source .= $self->header("archives", "Archives", $root);
+    #$source .= $self->header("archives", "Archives", $root);
     $source .= "<div class='section top' id='content'>
     <div class='wrapper'>
     <h1 class='cat'>Everything</h1>
@@ -297,7 +195,7 @@ sub archives {
     }
 
 
-    $source .= $self->footer();
+    #$source .= $self->footer();
 
 
     return $source;
@@ -308,7 +206,7 @@ sub error {
 
     my $source = "";
 
-    $source .= $self->header("error", "Page Not Found", $root);
+    #$source .= $self->header("error", "Page Not Found", $root);
 
     $source .= "<div class='section'>
     <div class='wrapper'>
@@ -329,7 +227,7 @@ sub error {
 
     </div>";
 
-    $source .= $self->footer();
+    #$source .= $self->footer();
 
     return $source;
 }
@@ -338,11 +236,11 @@ sub main {
     my ($self, $page) = @_;
     my $source = "";
 
-    $source .= $self->header("about", $page->meta("title"), $page->meta("root"));
+    #$source .= $self->header("about", $page->meta("title"), $page->meta("root"));
 
     $source .= $page->content;
 
-    $source .= $self->footer();
+    #$source .= $self->footer();
     return $source;
 }
 
