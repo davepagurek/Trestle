340fc832ab6d145b66acfb591b7b8b32c7e04dbc
Dave Pagurek <dave@davepagurek.com>
2015-02-13 22:08:41 -0500

    Templates for admin pages

--- admin/index.pl
+++ admin/index.pl
@@ -7,52 +7,17 @@ use Digest::MD5 qw(md5);
 use File::Find;
 use File::Path qw(rmtree);
 use HTML::Entities;
+use HTML::Template;
 use Encode;
 use strict;
 
 
-my $loggedin = 0;
-
-sub header {
-    my ($title) = @_;
-    my $output = "<!DOCTYPE html>
-    <html>
-    <head>
-        <title>Admin - " . $title . "</title>
-        <link rel='stylesheet' type='text/css' href='style.css' />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700' rel='stylesheet' type='text/css'>";
-
-    if (lc($title) eq "editor") {
-        $output .= "<script type='text/javascript' src='editor.js'></script>";
-    } elsif (lc($title) eq "dashboard") {
-        $output .= "<script type='text/javascript' src='dashboard.js'></script>";
-    }
-
-    $output .= "
-    </head>
-    <body>
-        <div id='header'>
-            <h1><a href='index.pl'>Trestle Admin</a></h1>";
-
-    if ($loggedin) {
-        $output .="
-            <form method='post' id='logout'>
-                <input type='hidden' name='log_out' id='log_out' value='true' />
-                <input type='submit' value='Log Out' />
-            </form>";
-    }
-    $output .= "</div><div class='container'>";
-    return $output;
-}
-
-sub footer {
-    my $output = "</div></body>
-    </html>";
-    return $output;
-}
+my $loggedIn = 0;
+my $templateDir = "templates";
 
 my @files = ();
 my $dirs = {};
+
 sub wanted {
     if ($File::Find::name =~ /content\/(.*\/)?(.+\.html)$/i) {
         if (length($1) == 0) {
@@ -66,6 +31,7 @@ sub wanted {
         }
     }
 }
+
 sub preprocess {
     sort {  uc $a cmp uc $b } @_;
 }
@@ -81,7 +47,7 @@ my $message = "";
 if ($query->param("log_out")) {
     $session->clear(["logged_in"]);
 } elsif ($session->param("logged_in") && $session->param("key") eq md5($credentials{password} . $credentials{key})) {
-    $loggedin=1;
+    $loggedIn=1;
 } elsif ($query->param("username") eq $credentials{username} && $query->param("password") eq $credentials{password}) {
     $session->param("logged_in", "true");
     $session->param("key", md5($credentials{password} . $credentials{key}));
@@ -90,14 +56,15 @@ if ($query->param("log_out")) {
         -type => "text.html",
         -cookie => $cookie
     );
-    $loggedin=1;
+    $loggedIn=1;
 } elsif ($query->param("username") || $query->param("password")) {
     $message = "Incorrect login.";
 }
 
 print $query->header("text/html") unless $query->{".header_printed"};
 
-if ($loggedin) {
+
+if ($loggedIn) {
 
     #extend expiration
     $session->expire('+2h');
@@ -105,122 +72,102 @@ if ($loggedin) {
     if ($query->url_param("edit") && -e "../content/" . $query->url_param("edit")) {
 
         my $source = "../content/" . $query->url_param("edit");
-
-
         if ($query->param("content")) {
             open my $content, ">", $source or die "Can't open $source: $!";
             my $decoded = $query->param("content");
             $decoded =~ s/\r//g;
             print $content $decoded;
             close $content;
+            $message = "File saved successfully";
         }
 
-        print header("Editor");
-
-
-        print "
-        <div class='full'>
-            <form id='editor' method='post'>
-                <textarea id='content' name='content'>";
-
+        my $pageContent = "";
         open my $content, "<", $source or die "Can't open $source: $!";
         while (<$content>) {
-            print encode_entities($_);
+            $pageContent .= encode_entities($_);
         }
         close $content or die "can't close '$content': $!";
 
-
-        print "</textarea>
-                <input type='checkbox' id='spellcheck' name='spellcheck' checked> <label for='spellcheck'>Spellcheck</label>
-
-                <input type='submit' value='save' />
-            </form>
-            <iframe id='media' frameborder='0' src='uploader.pl'></iframe>
-        </div>"
+        my $template = HTML::Template->new(
+            filename => "$templateDir/editor.html",
+            die_on_bad_params =>  0
+        );
+        $template->param({
+            title => "Editor: " . $query->url_param("edit"),
+            loggedIn => $loggedIn,
+            editor => 1,
+            message => $message,
+            source => $pageContent
+        });
+        print $template->output;
 
 
     } else {
-
-        find ({
-            "wanted" => \&wanted,
-            "preprocess" => \&preprocess
-        }, "../content");
-
-        print header("Dashboard");
-
-        print "
-        <div class='section'><div class='wrapper'>
-            <h2>Edit Page</h2>
-            <ul class='dirlist'>";
-
-        for my $file (@files) {
-            print "<li><a href='?edit=$file'>$file</a></li>";
-        }
-
-        for my $dir (sort keys %$dirs) {
-            print "<li class='dir closed'><span>$dir</span><ul>";
-
-            for my $file (@{ $dirs->{$dir} }) {
-                print "<li><a href='?edit=$dir/$file'>$file</a></li>";
-            }
-
-            print "</ul></li>"
-        }
-
-        print "
-            </ul>
-        </div></div>
-        <div class='section'><div class='wrapper'>
-            <h2>Management</h2>
-            <form method='post' id='clearCache'>
-                <input type='hidden' name='clear_cache' id='clear_cache' value='true' />
-                <input type='submit' value='Clear Cache' />
-            </form>
-            <form method='post' id='restartServer'>
-                <input type='hidden' name='restart_server' id='restart_server' value='true' />
-                <input type='submit' value='Restart Trestle' />
-            </form>
-        ";
         if ($query->param("clear_cache") && $query->param("clear_cache") eq "true") {
             rmtree("../cache");
-            print "<p class='message'>Cache cleared successfully.</p>"
+            $message = "Cache cleared successfully.";
         }
         if ($query->param("restart_server") && $query->param("restart_server") eq "true") {
             open (my $rebuild, ">", "../rebuild") or die "Could not open rebuild: $!";
             print $rebuild "1";
             close($rebuild);
-            print "<p class='message'>Server restarted successfully.</p>"
+            $message = "Server set to restart on next request.";
         }
-        print "</div></div>";
 
-        print footer();
+        #Make file list
+        my $files = [];
+        find ({
+            "wanted" => \&wanted,
+            "preprocess" => \&preprocess
+        }, "../content");
+
+        for my $file (@files) {
+            push(@{ $files }, {
+                fileName => $file,
+            });
+        }
+
+        for my $dir (sort keys %$dirs) {
+            my $dirFiles = [];
+
+            for my $file (@{ $dirs->{$dir} }) {
+                push(@{ $dirFiles }, {
+                    fileName => $file,
+                    dirName => $dir
+                });
+            }
+
+            push(@{ $files }, {
+                dirName => $dir,
+                dirFiles => $dirFiles
+            })
+        }
+        my $template = HTML::Template->new(
+            filename => "$templateDir/dashboard.html",
+            die_on_bad_params =>  0
+        );
+        $template->param({
+            title => "Dashboard",
+            loggedIn => $loggedIn,
+            dashboard => 1,
+            message => $message,
+            files => $files
+        });
+        print $template->output;
+
     }
 } else {
-    #present login format
+    #present login form
+    my $template = HTML::Template->new(
+        filename => "$templateDir/login.html",
+        die_on_bad_params =>  0
+    );
+    $template->param({
+        title => "Login",
+        message => $message,
+    });
+    print $template->output;
 
-    print header("Log In");
-    print "
-        <h1>Trestle Login</h1>";
-
-    if ($message) {
-        print "<p class='message'>$message</p>";
-    }
-
-    print "
-        <form method='post' id='login'>
-            <div class='line'>
-                <label for='username'>Username</label>
-                <input type='text' name='username' id='username' />
-            </div>
-            <div class='line'>
-                <label for='password'>Password</label>
-                <input type='password' name='password' id='password' />
-            </div>
-
-            <input type='submit' value='Log In' />
-        </form>";
-
-    print footer();
 }
 
 
