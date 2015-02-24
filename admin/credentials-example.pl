aa8d507a839b408564fe2d01c527e7b6f66f2c88
Dave Pagurek <dave@davepagurek.com>
2015-02-23 23:09:44 -0500

    Change credentials example to new format

--- admin/credentials-example.pl
+++ admin/credentials-example.pl
@@ -1,5 +1,10 @@
 #Rename this file to "credentials.pl" and create actual login credentials.
-
-username => "test",
-password => "test",
-key => "test346374725762572457F"
+(
+    username => "test",
+    password => "test",
+    gitname => "John Doe",
+    gitemail => "test\@test.com",
+    gitusername => "johndoe",
+    gitpassword => "test",
+    key => "test346374725762572457F"
+);
