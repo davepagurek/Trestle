faa8d3d233e8220afcebcdf9c6fe7876b2f0f02a
Dave Pagurek <dave@davepagurek.com>
2015-06-20 13:04:59 -0400

    Use Moose for Trestle

--- Trestle.pm
+++ Trestle.pm
@@ -1,196 +1,247 @@
-package Trestle;
+package Trestle {
 
-use CGI;
-use CGI::Fast;
+    use CGI;
+    use CGI::Fast;
 
-use Trestle::Page;
-use Trestle::Category;
-use strict;
+    use Trestle::Page;
+    use Trestle::Category;
+    use Trestle::Theme;
 
-sub new {
-    my $self = {};
-    my $class = shift;
-    my $config = shift;
-    $self->{config} = $config;
+    use Moose;
 
+    has "dev" => (
+        is => "ro",
+        isa => "Bool"
+    );
 
-    bless $self, $class;
-    return $self;
-}
+    has "root" => (
+        is => "ro",
+        isa => "Str"
+    );
 
-sub run {
-    my $self = shift;
+    has "theme" => (
+        is => "rw",
+        isa => "Object"
+    );
 
-    if ($self->{config}->{dev}) {
-        require CGI::Carp;
-        CGI::Carp->import(qw(warningsToBrowser fatalsToBrowser)) if CGI::Carp -> can ("import");
+    has "plugins" => (
+        is => "rw",
+        isa => "ArrayRef"
+    );
+
+    has "cacheLife" => (
+        is => "rw",
+        isa => "Int"
+    );
+
+    has "query" => (
+        is => "ro",
+        isa => "CGI",
+        init_arg => undef,
+        writer => "_next_query"
+    );
+
+    sub run {
+        my $self = shift;
+
+        if ($self->dev) {
+            require CGI::Carp;
+            CGI::Carp->import(qw(warningsToBrowser fatalsToBrowser)) if CGI::Carp -> can ("import");
+        }
+
+        while (my $q = CGI::Fast->new) {
+
+            $self->_next_query(CGI->new());
+
+            for ($self->_pageName) {
+                if    ($self->_showCache($_))   {  }
+                elsif ($_ eq "index")           { $self->_showIndex; }
+                elsif ($_ eq "archives")        { $self->_showArchives; }
+                elsif ($self->_isPage($_))      { $self->_showPage($_) }
+                elsif ($self->_isCategory($_))  { $self->_showCategory($_) }
+                else                            { $self->_showError($_) }
+            }
+
+            if (-e "rebuild") {
+                unlink "rebuild" or die "Unable to unlink rebuild: $!";
+                last;
+            }
+        }
     }
 
-    while (my $q = CGI::Fast->new) {
+    sub cache {
+        my $self = shift;
+        my $pageName = shift;
+        my $content = shift;
 
+        $content = "<!-- " . time . " -->\n" . $content;
 
-        my $query = CGI->new();
-        my $pageName = $query->param("page") || "index";
-        $pageName =~ s/\/+$//;
+        my $dir = "";
+        $dir = $1 if $pageName =~ /^(.*)\/[\w-]/;
+
+        my $cacheDir = "cache/" . $dir;
+        if (!-d "cache") {
+            mkdir "cache" or die "Unable to create cache: $1";
+        }
+        if (!-d $cacheDir) {
+            mkdir $cacheDir or die "Unable to create $cacheDir: $!";
+        }
+
+        my $sourceCache = "cache/" . $pageName . "_cache.html";
+        open my $cached, ">", $sourceCache or die "Can't open $sourceCache: $!";
+        print $cached $content;
+        close $cached or die "can't close $cached: $!";
+    }
+
+    sub _pageName {
+        my $self = shift;
+        my $pageName = $self->query->param("page") || "index";
+        $pageName =~ s/\/+$//; #Remove trailing slashes
+        return $pageName;
+    }
+
+    sub _isPage {
+        my $self = shift;
+        my $pageName = shift;
         my $source = "content/" . $pageName . ".html";
+
+        return -e $source;
+    }
+
+    sub _isCategory {
+        my $self = shift;
+        my $pageName = shift;
         my $sourceDir = "content/" . $pageName;
+
+        return $pageName ne "images" && -d $sourceDir
+    }
+
+    sub _showCache {
+        my $self = shift;
+        my $pageName = shift;
         my $sourceCache = "cache/" . $pageName . "_cache.html";
 
-        #Check if cache exists
-        my $remake = 0;
-        if (-e $sourceCache) {
+        return 0 unless -e $sourceCache;
 
-            #Read the timestamp from the first line
-            open my $cached, "<", $sourceCache or die "Can't open $source: $!";
-            my $input = <$cached>;
-            my $created = 0;
-            if ($input =~ /<\!-- ([0-9]+) -->/) {
-                $created = $1;
-            }
-
-            #Print the whole cached file if the cache's life hasn't expired
-            if (time-$created <= $self->{config}->{cacheLife}*60*60) {
-                print $query->header("text/html");
-                while (<$cached>) {
-                    print $_;
-                }
-
-                #Otherwise mark the post to be re-cached
-            } else {
-                $remake = 1;
-            }
-            close $cached or die "can't close '$cached': $!";
-        } else {
-            $remake = 1;
+        #Read the timestamp from the first line
+        open my $cached, "<", $sourceCache or die "Can't open $sourceCache: $!";
+        my $input = <$cached>;
+        my $created = 0;
+        if ($input =~ /<\!-- ([0-9]+) -->/) {
+            $created = $1;
         }
 
-        if ($remake) {
+        #Print the whole cached file if the cache's life hasn't expired
+        close $cached && return 0 unless time-$created <= $self->cacheLife*60*60;
 
-            #make cache subdirectory if it doesn't exist
-            my $dir = "";
-            $dir = $1 if $pageName =~ /^(.*)\/[\w-]/;
-
-
-            my $content = "";
-            my $cache = 0;
-
-            if ($pageName eq "index") {
-                print $query->header("text/html");
-                $cache = 1;
-
-                #Create index page
-                my $page = Trestle::Page->new($source, $self->{config}->{root});
-
-                #Get the theme's output for the index page
-                $content .= $self->{config}->{theme}->main($page);
-
-                #Run the output through plugins that can edit index pages
-                for (my $i=0; $i<scalar @{$self->{config}->{plugins}}; $i++) {
-                    if ($self->{config}->{plugins}[$i]->{index}) {
-                        $content = $self->{config}->{plugins}[$i]->content($content, $page);
-                    }
-                }
-
-            } elsif ($pageName eq "archives") {
-                print $query->header("text/html");
-                $cache = 1;
-
-                #Create a list of category directories
-                my @categories = ();
-                opendir(my $dh, "content") || die "can't opendir content: $!";
-                my @dirs = grep {-d "content/$_" && ! /^\./} readdir($dh);
-
-                #Create a category page for each
-                for my $dir (@dirs) {
-                    if ($dir ne "images") {
-                        push(@categories, Trestle::Category->new("content/" . $dir, $self->{config}->{root}));
-                    }
-                }
-
-                #Get the theme's output for the archives page
-                $content .= $self->{config}->{theme}->archives($self->{config}->{root}, @categories);
-
-                #Run the output through plugins that can edit archives pages
-                for (my $i=0; $i<scalar @{$self->{config}->{plugins}}; $i++) {
-                    if ($self->{config}->{plugins}[$i]->{archives}) {
-                        $content = $self->{config}->{plugins}[$i]->content($content, @categories);
-                    }
-                }
-
-                #If the source file exists, display the page
-            } elsif (-e $source) {
-                print $query->header("text/html");
-                $cache = 1;
-
-                #Create page
-                my $page = Trestle::Page->new($source, $self->{config}->{root});
-
-                #Get the theme's output for the page
-                $content .= $self->{config}->{theme}->content($page);
-
-                #Run the output through plugins that can edit index pages
-                for (my $i=0; $i<scalar @{$self->{config}->{plugins}}; $i++) {
-                    if ($self->{config}->{plugins}[$i]->{pages}) {
-                        $content = $self->{config}->{plugins}[$i]->content($content, $page);
-                    }
-                }
-
-                #If the directory exists, display it as a category (excluding the images directory)
-            } elsif ($pageName ne "images" && -d $sourceDir) {
-                print $query->header("text/html");
-                $cache = 1;
-
-                #Create category page
-                my $category = Trestle::Category->new($sourceDir, $self->{config}->{root});
-
-                #Get the theme's output for the category page
-                $content .= $self->{config}->{theme}->dir($category);
-
-                #Run the output through plugins that can edit index pages
-                for (my $i=0; $i<scalar @{$self->{config}->{plugins}}; $i++) {
-                    if ($self->{config}->{plugins}[$i]->{categories}) {
-                        $content = $self->{config}->{plugins}[$i]->content($content, $category);
-                    }
-                }
-
-                #Otherwise, it's an error
-            } else {
-                print $query->header( -status => '404 Not Found' );
-
-                #Get the theme's output for the error page
-                $content .= $self->{config}->{theme}->error(404, $self->{config}->{root});
-
-                #Run the output through plugins that can edit 404 error pages
-                for (my $i=0; $i<scalar @{$self->{config}->{plugins}}; $i++) {
-                    if ($self->{config}->{plugins}[$i]->{error}) {
-                        $content = $self->{config}->{plugins}[$i]->content($content, 404);
-                    }
-                }
-            }
-
-            #Print the page
-            print $content;
-
-            #Add a timestamp and save the cached version
-            $content = "<!-- " . time . " -->\n" . $content;
-
-            if ($cache) {
-                my $cacheDir = "cache/" . $dir;
-                if (!-d "cache") {
-                    mkdir "cache" or die "Unable to create cache: $1";
-                }
-                if (!-d $cacheDir) {
-                    mkdir $cacheDir or die "Unable to create $cacheDir: $!";
-                }
-                open my $cached, ">", $sourceCache or die "Can't open $sourceCache: $!";
-                print $cached $content;
-                close $cached or die "can't close '$cached': $!";
-            }
+        print $self->query->header("text/html");
+        while (<$cached>) {
+            print $_;
         }
-        if (-e "rebuild") {
-            unlink "rebuild" or die "Unable to unlink rebuild: $!";
-            last;
+        close $cached && return 1 or die "can't close '$cached': $!";
+    }
+
+    sub _showIndex {
+        my $self = shift;
+
+        #Create index page
+        my $page = Trestle::Page->new("content/index.html", $self->root);
+
+        #Get the theme's output for the index page
+        my $content = $self->theme->main($page);
+
+        #Run the output through plugins that can edit index pages
+        for my $plugin (grep {$_->{index}} @{$self->plugins}) {
+            $content = $plugin->content($content, $page);
         }
+
+        $self->cache(index => $content);
+
+        print $self->query->header("text/html");
+        print $content;
+    }
+
+    sub _showArchives {
+        my $self = shift;
+
+        #Create a list of category directories
+        my @categories = ();
+        opendir(my $dh, "content") || die "can't opendir content: $!";
+        my @dirs = grep {-d "content/$_" && ! /^\./ && $_ ne "images"} readdir($dh);
+
+        #Create a category page for each
+        for my $dir (@dirs) {
+            push(@categories, Trestle::Category->new("content/" . $dir, $self->root));
+        }
+
+        #Get the theme's output for the archives page
+        my $content = $self->theme->archives($self->root, @categories);
+
+        #Run the output through plugins that can edit archives pages
+        for my $plugin (grep {$_->{archives}} @{$self->plugins}) {
+            $content = $plugin->content($content, @categories);
+        }
+
+        $self->cache(archives => $content);
+
+        print $self->query->header("text/html");
+        print $content;
+    }
+
+    sub _showPage {
+        my $self = shift;
+        my $pageName = shift;
+
+        #Create page
+        my $page = Trestle::Page->new("content/" . $pageName . ".html", $self->root);
+
+        #Get the theme's output for the page
+        my $content = $self->theme->content($page);
+
+        #Run the output through plugins that can edit pages
+        for my $plugin (grep {$_->{pages}} @{$self->plugins}) {
+            $content = $plugin->content($content, $page);
+        }
+
+        $self->cache($pageName => $content);
+        print $self->query->header("text/html");
+        print $content;
+    }
+
+    sub _showCategory {
+        my $self = shift;
+        my $pageName = shift;
+
+        #Create category page
+        my $category = Trestle::Category->new("content/$pageName", $self->root);
+
+        #Get the theme's output for the category page
+        my $content = $self->theme->dir($category);
+
+        #Run the output through plugins that can edit category pages
+        for my $plugin (grep {$_->{categories}} @{$self->plugins}) {
+            $content = $plugin->content($content, $category);
+        }
+
+        $self->cache($pageName => $content);
+        print $self->query->header("text/html");
+        print $content;
+    }
+
+    sub _showError {
+        my $self = shift;
+        my $pageName = shift;
+
+        #Get the theme's output for the error page
+        my $content = $self->theme->error(404, $self->root);
+
+        #Run the output through plugins that can edit 404 error pages
+        for my $plugin (grep {$_->{error}} @{$self->plugins}) {
+            $content = $plugin->content($content, 404);
+        }
+
+        print $self->query->header( -status => '404 Not Found' );
+        print $content;
     }
 }
 
--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
+requires 'Moose';
 requires 'FCGI';
 requires 'JSON';
 requires 'HTML::Template';
